74198a899ed2b5cf010d2b1d3ab4a5fabaab4f47
Etativel <farhanmaulana4611@gmail.com>
2023-10-14 20:25:49 +0700

    adding boardsize function

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    createDrawBoard(64) 
-    console.log("hello")
-})
-
+let drawBoard = document.querySelector(".grid");
 
 function createDrawBoard(size){
-    let drawBoard = document.querySelector(".grid");
 
     drawBoard.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     drawBoard.style.gridTemplateRows = `repeat(${size}, 1fr)`
@@ -14,6 +9,41 @@ function createDrawBoard(size){
 
     for (let i = 0; i < numDivs; i++){
         let div = document.createElement("div");
-        drawBoard.insertAdjacentElement("beforeend", div)
+        div.addEventListener('mouseover',()=>{
+            div.style.backgroundColor = 'black'
+        })
+        drawBoard.appendChild(div)
     }
 }
+
+
+const boardSize = document.querySelector(".boardSize")
+let currentBoardSize;
+
+
+boardSize.addEventListener('click', (ev)=>{
+    let target = ev.target
+    let child = Array.from(drawBoard.childNodes);
+    switch (target.id){
+        case "board-16":
+            child.forEach((item)=>drawBoard.removeChild(item))
+            createDrawBoard(16);
+            currentBoardSize = 16
+            break;
+        case "board-32":
+            child.forEach((item)=>drawBoard.removeChild(item))
+            createDrawBoard(32);
+            currentBoardSize = 32
+            break;
+        case "board-64":
+            child.forEach((item)=>drawBoard.removeChild(item))
+            createDrawBoard(64);
+            currentBoardSize = 64
+            break;
+    }
+})
+
+
+createDrawBoard(16)
+
+console.log(currentBoardSize)
